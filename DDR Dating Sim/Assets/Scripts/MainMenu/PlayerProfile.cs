32d90efc1fff902d466448e1b78120004277e988
ReisalinStout <103336978+ReisalinStout@users.noreply.github.com>
2022-05-31 14:05:39 -0700

    Add a missing file
    This file was required by another file, but since save system is will not be implemented this time around I made sure this doesn't cause errors.

---
+++ DDR Dating Sim/Assets/Scripts/MainMenu/PlayerProfile.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProfile
+{
+    public int sceneNumber;
+    public int dialogueNumber;
+    public int score;
+}
