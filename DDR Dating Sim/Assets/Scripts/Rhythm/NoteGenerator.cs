e1e2a93f2b4b09dac4f1656237b7cbe5f09e233b
PENTASHIFT <jwmunsey1@gmail.com>
2022-11-16 00:21:10 -0800

    Almost working

--- DDR Dating Sim/Assets/Scripts/Rhythm/NoteGenerator.cs
+++ DDR Dating Sim/Assets/Scripts/Rhythm/NoteGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,40 +13,105 @@ public class NoteGenerator : MonoBehaviour
     void Start()
     {
         beat_tempo = GetComponent<BeatScroller>().beat_tempo;
+        Debug.Log(beat_tempo);
 
-        // Read from file and generate here.
-        GenerateLeft(0.0f);
-        GenerateUp(-1.0f);
-        GenerateDown(-2.0f);
-        GenerateRight(-3.0f);
+        ParseFile();
     }
 
-    string[,] ParseFile()
+    void ParseFile()
     {
         // Parse CSV file here.
 
         string[] lines = csv.ToString().Split('\n');
-        string[,] beatMap = new string[lines.Length - 1, 4];
+        float currentYPos = 0.0f;
 
         for (int i = 0; i < lines.Length - 1; i++)
         {
-            string[] vals = lines[i].Split(',');
+            string[] val = lines[i].Split(',');
+            string note = val[0];
+            float deltaTime = (float)Convert.ToDouble(val[1]);
 
-            for (int ii = 0; ii < vals.Length - 1; ii++)
+            // TODO(josh): Don't check for specific note numbers.
+            switch(note)
             {
-                beatMap[i, ii] = vals[ii];
+                case "59":
+                case "80":
+                {
+                    Debug.Log("Right");
+                    if (0.0f == deltaTime)
+                    {
+                        GenerateRight(currentYPos);
+                    }
+                    else
+                    {
+                        //currentYPos -= beat_tempo / deltaTime;
+                        //currentYPos -= beat_tempo * deltaTime;
+                        currentYPos -= (deltaTime * 4f);
+                        GenerateRight(currentYPos);
+                    }
+                } break;
+                
+                case "57":
+                case "78":
+                {
+                    Debug.Log("Down");
+                    if (0.0f == deltaTime)
+                    {
+                        GenerateDown(currentYPos);
+                    }
+                    else
+                    {
+                        //currentYPos -= beat_tempo / deltaTime;
+                        //currentYPos -= beat_tempo * deltaTime;
+                        currentYPos -= (deltaTime * 4f);
+                        GenerateDown(currentYPos);
+                    }
+                } break;
+                
+                case "55":
+                case "76":
+                {
+                    Debug.Log("Up");
+                    if (0.0f == deltaTime)
+                    {
+                        GenerateUp(currentYPos);
+                    }
+                    else
+                    {
+                        //currentYPos -= beat_tempo / deltaTime;
+                        //currentYPos -= beat_tempo * deltaTime;
+                        currentYPos -= (deltaTime * 4f);
+                        GenerateUp(currentYPos);
+                    }
+                } break;
+
+                case "53":
+                case "74":
+                {
+                    Debug.Log("Left");
+                    if (0.0f == deltaTime)
+                    {
+                        GenerateLeft(currentYPos);
+                    }
+                    else
+                    {
+                        //currentYPos -= beat_tempo / deltaTime;
+                        //currentYPos -= beat_tempo * deltaTime;
+                        currentYPos -= (deltaTime * 4f);
+                        GenerateLeft(currentYPos);
+                    }
+                } break;
+                
+                default: break;
             }
         }
-
-        return beatMap;
     }
 
     void GenerateLeft(float y)
     {
         Vector3 pos = new Vector3(-1.5f, y, 0.0f);
         Vector3 rotation = new Vector3(0.0f, 0.0f, 180.0f);
-        GameObject newArrow = Instantiate(arrow, pos, Quaternion.Euler(rotation));
-        newArrow.transform.parent = gameObject.transform;
+        GameObject newArrow = Instantiate(arrow, pos, Quaternion.Euler(rotation), gameObject.transform);
         newArrow.GetComponent<NoteBehavior>().key_to_press = KeyCode.S;
         newArrow.GetComponent<SpriteRenderer>().enabled = true;
     }
@@ -54,8 +120,7 @@ public class NoteGenerator : MonoBehaviour
     {
         Vector3 pos = new Vector3(-0.5f, y, 0.0f);
         Vector3 rotation = new Vector3(0.0f, 0.0f, 90.0f);
-        GameObject newArrow = Instantiate(arrow, pos, Quaternion.Euler(rotation));
-        newArrow.transform.parent = gameObject.transform;
+        GameObject newArrow = Instantiate(arrow, pos, Quaternion.Euler(rotation), gameObject.transform);
         newArrow.GetComponent<NoteBehavior>().key_to_press = KeyCode.D;
         newArrow.GetComponent<SpriteRenderer>().enabled = true;
     }
@@ -64,8 +129,7 @@ public class NoteGenerator : MonoBehaviour
     {
         Vector3 pos = new Vector3(0.5f, y, 0.0f);
         Vector3 rotation = new Vector3(0.0f, 0.0f, -90.0f);
-        GameObject newArrow = Instantiate(arrow, pos, Quaternion.Euler(rotation));
-        newArrow.transform.parent = gameObject.transform;
+        GameObject newArrow = Instantiate(arrow, pos, Quaternion.Euler(rotation), gameObject.transform);
         newArrow.GetComponent<NoteBehavior>().key_to_press = KeyCode.J;
         newArrow.GetComponent<SpriteRenderer>().enabled = true;
 
@@ -75,8 +139,7 @@ public class NoteGenerator : MonoBehaviour
     {
         Vector3 pos = new Vector3(1.5f, y, 0.0f);
         Vector3 rotation = new Vector3(0.0f, 0.0f, 0.0f);
-        GameObject newArrow = Instantiate(arrow, pos, Quaternion.Euler(rotation));
-        newArrow.transform.parent = gameObject.transform;
+        GameObject newArrow = Instantiate(arrow, pos, Quaternion.Euler(rotation), gameObject.transform);
         newArrow.GetComponent<NoteBehavior>().key_to_press = KeyCode.K;
         newArrow.GetComponent<SpriteRenderer>().enabled = true;
     }
